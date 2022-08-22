e4f4d72bdbc3fb5d255b2ea9c8b4e16e4851f29e
martiusx <Maciej.Cis@o2.pl>
2022-08-22 22:06:33 +0200

    created name form/saving name to localStorage/checking that the name is given

---
+++ src/nameForm/NameForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+
+export default function NameForm(){
+    const [name, setName] = useState('');
+
+    const handleNameChange = (e)=>{
+        const ss = e.target.value.replace(/\s/g, "");
+        setName(ss);
+    }
+
+    const handleNameSave = (e)=>{
+        localStorage.setItem("name", JSON.stringify(name));
+    }
+
+    const savedName = JSON.parse(localStorage.getItem('name'));
+
+    if(savedName === null || savedName === ""){
+        return(
+            <form>
+                <input type="text" value={name} onChange={handleNameChange}>
+                </input>
+                <input type="submit" value="send" onClick={handleNameSave}></input>
+            </form>
+        )
+    }else{
+        return(
+            <h1>WITAJ {savedName}</h1>
+        )
+    }
+}
